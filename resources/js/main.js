99f7a9cad31a7c00d63e84e16badee34a4640072
codermim <aysha6966044@gmail.com>
2022-11-28 09:33:07 -0500

    first commit

---
+++ resources/js/main.js
@@ -0,0 +1,14 @@
+// MENU
+function openMenu() {
+    document.getElementById('hb-menu') .style.width = "100%";
+    
+}
+function closeMenu() {
+    document.getElementById('hb-menu') .style.width = "0";
+}
+// mix it up
+var mixer = mixitup('.work-grid');
+
+// Wow 
+new WOW().init();
+  
